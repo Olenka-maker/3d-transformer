18edeb01df8ee89f505743e2fe8cf60c2ae0bdd6
Olenka-maker <olenkachmil14@gmail.com>
2025-09-02 21:07:32 +0300

    done 3d

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,11 @@
-console.log(12)
+let el1 = document.querySelector('.olena');
+
+document.addEventListener('mousemove', function(e){
+    let x = e.pageX - window.innerWidth / 2;
+    let y = e.pageY - window.innerHeight / 2;
+
+    let angleX = (20 * x) / (window.innerWidth / 2);
+    let angleY = (20 * y) / (window.innerHeight / 2);
+
+    el1.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`;
+});
